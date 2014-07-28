9154565c50ecec7137e4cb9269474abefdda7705
Jonathan Rochkind <jonathan@dnil.net>
2014-07-28 11:46:28 -0400

    refactored js

--- js/viewer.js
+++ js/viewer.js
@@ -1,62 +1,57 @@
 /* All the javascript we need for the poster viewer in one file */
 
+
+/* Used by actual on-screen annotorious make annotation stuff, which
+   we're not really using at present */
 function annotate() {
-        var button = document.getElementById('map-annotate-button');
-        button.style.color = '#777';
+  var button = document.getElementById('map-annotate-button');
+  button.style.color = '#777';
 
-        anno.activateSelector(function() {
-          // Reset button style
-          button.style.color = '#fff';
-        });
-      }
-
-      function init() {
-
-annotorious.plugin.HelloWorldPlugin = function(opt_config_options) { }
-
-annotorious.plugin.HelloWorldPlugin.prototype.initPlugin = function(anno) {
-  // Add initialization code here, if needed (or just skip this method if not)
-
-    anno.addHandler('onMouseOverAnnotation', function(ann) {
-      return true;
-    });
-
-}
-
-annotorious.plugin.HelloWorldPlugin.prototype.onInitAnnotator = function(annotator) {
-  // A Field can be an HTML string or a function(annotation) that returns a string
-  annotator.popup.addField(function(annotation) { 
-    return '<em>Hello World: ' + annotation.text.length + ' chars</em>'
+  anno.activateSelector(function() {
+    // Reset button style
+    button.style.color = '#fff';
   });
 }
 
-// Add the plugin like so
-anno.addPlugin('HelloWorldPlugin', {});
+/* Just a demo, not really using */
+function addHelloWorldPlugin() {
 
+  annotorious.plugin.HelloWorldPlugin = function(opt_config_options) { }
 
-        var viewer = OpenSeadragon({
-          id: "openseadragon",
-          //prefixUrl: "http://annotorious.github.io/js/openseadragon/images/",
-          showNavigator: false,
-          prefixUrl: "/beehive/openseadragon/images/",
-          tileSources: "/beehive/MR_WholePoster_PrintRes/mr_zoom.dzi"
+  annotorious.plugin.HelloWorldPlugin.prototype.initPlugin = function(anno) {
+    // Add initialization code here, if needed (or just skip this method if not)
 
-         
-        });
-        
-        anno.makeAnnotatable(viewer);
-
-
-
-
-
-
-
-
-      }
-
-
-
-      jQuery( document ).ready(function( $ ) {
-        init();
+      anno.addHandler('onMouseOverAnnotation', function(ann) {
+        return true;
       });
+
+  }
+
+  annotorious.plugin.HelloWorldPlugin.prototype.onInitAnnotator = function(annotator) {
+    // A Field can be an HTML string or a function(annotation) that returns a string
+    annotator.popup.addField(function(annotation) { 
+      return '<em>Hello World: ' + annotation.text.length + ' chars</em>'
+    });
+  }
+
+  // Add the plugin like so
+  anno.addPlugin('HelloWorldPlugin', {});
+}
+
+function setupOpenSeadragonViewer() {
+  var viewer = OpenSeadragon({
+    id: "openseadragon",
+    //prefixUrl: "http://annotorious.github.io/js/openseadragon/images/",
+    showNavigator: false,
+    prefixUrl: "/beehive/openseadragon/images/",
+    tileSources: "/beehive/MR_WholePoster_PrintRes/mr_zoom.dzi"
+  });
+
+  anno.makeAnnotatable(viewer);
+}
+
+
+jQuery( document ).ready(function( $ ) {
+  setupOpenSeadragonViewer();
+  addHelloWorldPlugin();
+});
