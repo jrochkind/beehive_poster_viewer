d47dca40c9f8aa8a5d99f729d4169d2a298d7da9
Jonathan Rochkind <jonathan@dnil.net>
2014-08-03 16:30:38 -0400

    annotorious only in an admin mode

--- js/viewer.js
+++ js/viewer.js
@@ -62,46 +62,7 @@ function setPosterAndLang() {
 }
 
 
-/* Used by actual on-screen annotorious make annotation stuff, which
-   we're not really using at present */
-function annotate() {
-  var button = document.getElementById('map-annotate-button');
-  button.style.color = '#777';
 
-  anno.activateSelector(function() {
-    // Reset button style
-    button.style.color = '#fff';
-  });
-}
-
-/* Just a demo, not really using */
-function addHelloWorldPlugin() {
-
-  annotorious.plugin.HelloWorldPlugin = function(opt_config_options) { }
-
-  annotorious.plugin.HelloWorldPlugin.prototype.initPlugin = function(anno) {
-    // Add initialization code here, if needed (or just skip this method if not)
-  }
-
-  annotorious.plugin.HelloWorldPlugin.prototype.onInitAnnotator = function(annotator) {
-    // A Field can be an HTML string or a function(annotation) that returns a string
-    annotator.popup.addField(function(annotation) { 
-      var geometry = annotation.shapes[0].geometry;
-
-      return '<pre>' +
-        '&lt;region\n' +
-        '  x="' + geometry.x.toFixed(5) +  '"\n' +
-        '  y="' + geometry.y.toFixed(5) +  '"\n' +
-        '  width="' + geometry.width.toFixed(5) +  '"\n' +
-        '  height="' + geometry.height.toFixed(5) +  '"\n' +
-        '/&gt;' +
-        '</pre>';
-    });
-  }
-
-  // Add the plugin like so
-  anno.addPlugin('HelloWorldPlugin', {});
-}
 
 function setupOpenSeadragonViewer() {
   /* Save in global cause we're gonna need to refer to it and stuff 
@@ -124,8 +85,6 @@ function setupOpenSeadragonViewer() {
     homeButton: 'fullPosterBtn',
     fullPageButton: 'fullPageBtn'
   });
-
-  anno.makeAnnotatable(openSeadragonViewer);
 }
 
 function positionOverlayControls() {
@@ -479,11 +438,81 @@ function loadPosterData() {
     });
   });
 
+  // Add annotorious CSS and JS (which we currently only use for admin),
+  // add our custom Annotorious plugin, and show the 'define region' button
+  //
+  // We're currently linking to annotorious/latest on github, for some reason
+  // we had trouble using a local copy. 
+  function addAdminHelperUI() {
+    $("<link/>", {
+      rel: "stylesheet",
+      type: "text/css",
+      href: "http://annotorious.github.io/latest/annotorious.css"
+    }).appendTo("head");
+
+    $.ajax({
+      url: "http://annotorious.github.io/latest/annotorious.min.js",
+      dataType: "script",
+      success: function() {
+        // Important to add the plugin BEFORE we make the OpenSeadragon viewer
+        // annotatable
+        addHelloWorldPlugin();
+
+        anno.makeAnnotatable(openSeadragonViewer);
+
+        $("#map-annotate-button").show();
+      }
+    });
+
+  }
+  /* Used by actual on-screen annotorious make annotation stuff, which
+   we're not really using at present */
+function annotate() {
+  var button = document.getElementById('map-annotate-button');
+  button.style.color = '#777';
+
+  anno.activateSelector(function() {
+    // Reset button style
+    button.style.color = '#fff';
+  });
+}
+
+/* Just a demo, not really using */
+function addHelloWorldPlugin() {
+
+  annotorious.plugin.HelloWorldPlugin = function(opt_config_options) { }
+
+  annotorious.plugin.HelloWorldPlugin.prototype.initPlugin = function(anno) {
+    // Add initialization code here, if needed (or just skip this method if not)
+  }
+
+  annotorious.plugin.HelloWorldPlugin.prototype.onInitAnnotator = function(annotator) {
+    // A Field can be an HTML string or a function(annotation) that returns a string
+    annotator.popup.addField(function(annotation) { 
+      var geometry = annotation.shapes[0].geometry;
+
+      return '<pre>' +
+        '&lt;region\n' +
+        '  x="' + geometry.x.toFixed(5) +  '"\n' +
+        '  y="' + geometry.y.toFixed(5) +  '"\n' +
+        '  width="' + geometry.width.toFixed(5) +  '"\n' +
+        '  height="' + geometry.height.toFixed(5) +  '"\n' +
+        '/&gt;' +
+        '</pre>';
+    });
+  }
+
+  // Add the plugin like so
+  anno.addPlugin('HelloWorldPlugin', {});
+}
+
 
 
 setPosterAndLang();
 
-addHelloWorldPlugin();
+if (paramsToHash(document.location.search).admin === "true") {
+  addAdminHelperUI();
+}
 
 jQuery( document ).ready(function( $ ) {
   setupOpenSeadragonViewer();
